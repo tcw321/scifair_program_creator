dded99aa35fd25efc773cba3981ee28e146c5dc9
Timothy C. Wright <tcw321@gmail.com>
2012-02-04 21:40:09 -0500

    projects using project name and first student name

--- projects.py
+++ projects.py
@@ -5,10 +5,16 @@ class Entries(object):
     def find(self, key_value):
         name = key_value.values()[0]
         names = self.values['Teacher']
+        resultIndexes = []
         for i in range(len(names)):
             if names[i] == name:
-                break
-        return self.values['Project'][i]
+                resultIndexes.append(i)
+        results = []
+        for i in resultIndexes:
+            names = []
+            names.append(self.values['Student Name'][i])
+            results.append([self.values['Project Name'][i], names])
+        return results
 
 
 class TestEntriesClass(unittest.TestCase):
@@ -17,12 +23,14 @@ class TestEntriesClass(unittest.TestCase):
 
     entries = Entries()
     def setUp(self):
-        self.entries.values = {'Teacher': ['Chad', 'Mike', 'Denise'],
-                               'Project': ['ChadProject', 'MikeProject', 'DeniseProject']}
+        self.entries.values = {'Teacher': ['Chad', 'Mike', 'Denise', 'Denise'],
+                               'Project Name': ['ChadProject', 'MikeProject', 'DeniseProject', 'DeniseProject2'],
+                               'Student Name': ['C. Cod', 'M. Mad', 'D. Dump', 'D. Dail']} 
 
     def testFind(self):
         project = self.entries.find({"Teacher" : "Denise"})
-        self.assertEqual(project, "DeniseProject")
+        self.assertEqual(project, [["DeniseProject", ['D. Dump']], ["DeniseProject2", ['D. Dail']]])
+
         
 if __name__ == '__main__':
     unittest.main()
